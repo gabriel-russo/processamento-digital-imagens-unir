444875f119f34e9cdd165372600cb6455b86ad07
Gabriel Russo <gabrielrusso@protonmail.com>
2023-04-04 18:36:58 -0400

    Atividade 03

---
+++ Atividade_3/equalizarHistograma.m
@@ -0,0 +1,18 @@
+﻿function outImg = equalizarHistograma(img)
+% img: imagem de entrada
+
+% Calcula o histograma da imagem de entrada
+histo = zeros(256,1);
+for i = 1:size(img,1)
+  for j = 1:size(img,2)
+    histo(img(i,j)+1) = histo(img(i,j)+1) + 1;
+  end
+end
+
+% Calcula a função de distribuição acumulada (CDF) do histograma
+cdf = cumsum(histo) / numel(img);
+
+% Mapeia os valores de intensidade da imagem original para novos valores
+outImg = uint8(255 * cdf(img+1));
+
+end
---
+++ Atividade_3/expansao_histograma.m
@@ -0,0 +1,17 @@
+﻿function [img_saida] = expansao_histograma(img)
+    img = im2double(img);
+    [lin col] = size(img);
+    r_min = min(img(:));
+    L=5;
+    r_max = max(img(:));
+    img_saida = zeros(size(img));
+    for (i=1:lin)
+        for(j=1:col)
+            r= img(i,j);
+            %t = round(((r-r_min)/(r_max-r_min))*(L-1));
+            t = round(((r-r_min)*255)/(r_max-r_min));
+            img_saida(i,j) = t;
+        endfor
+    endfor
+    img_saida = uint8(img_saida);
+endfunction
